37b44c7c306a8896c7e70374bbffcb7631be1637
javixlive <javixlive@hotmail.com>
2022-08-16 12:34:45 -0500

    segunda prueba de tarea

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -7,7 +7,7 @@ const nuevaString = 'Esto es un String';
 const nuevoNum = 666;
 
 // Crea una variable booleana:
-const nuevoBool = True;
+const nuevoBool = true;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - 5 === 5;
